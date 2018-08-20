f19842e0dbe1b1715daa8098730814bed531b0b2
unknown <niicou96@hotmail.com>
2018-08-20 18:08:43 -0300

    Ventas Alta

--- DjangoInventario/compras/forms.py
+++ DjangoInventario/compras/forms.py
@@ -1,8 +1,13 @@
 from django.forms import ModelForm
-from .models import Producto
+from .models import Producto,Venta
+
 
 class ProductoForm(ModelForm):
     class Meta:
         model = Producto
         exclude = ['id']
 
+class VentaForm(ModelForm):
+    class Meta:
+        model = Venta
+        exclude = ['idVenta']
