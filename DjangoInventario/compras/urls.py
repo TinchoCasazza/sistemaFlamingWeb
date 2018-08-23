a5a26b09ce324357a121be33aa8e7a076ff9e724
martin <mcasazza@kaitzen-solutions.com>
2018-08-23 17:39:04 -0300

    Login

--- DjangoInventario/compras/urls.py
+++ DjangoInventario/compras/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from . import views
+from django.contrib.auth.views import login 
 
 # URLs del Modulo Compras 
 
 urlpatterns = [
-    url(r'^$', views.inicio, name = 'inicio_url'),
+    url(r'^$', login, {'template_name':'login.html'}, name = 'login'),
+    url(r'inicio/$', views.inicio, name = 'inicio_url'),
     url(r'productos/$', views.productos, name = 'productos_url'),
     url(r'producto_form/$', views.Alta_Producto, name = 'ProductoAlta_url'),
     url(r'proveedores/$', views.proveedores, name = 'proveedores_url'),
